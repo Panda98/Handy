be7c533a60f36396bbe99f5607b61b3b64166296
Panda98 <810650217@qq.com>
2019-04-12 19:36:52 +0800

    添加User service测试

--- Background/HandyBack/src/test/java/UserServiceTest.java
+++ Background/HandyBack/src/test/java/UserServiceTest.java
@@ -1,9 +1,16 @@
+import com.handy.support.entity.User;
 import com.handy.support.pojo.user.dto.UserDto;
+import com.handy.support.pojo.user.vo.UserAuthorVO;
+import com.handy.support.pojo.user.vo.UserLabelVO;
+import com.handy.support.pojo.user.vo.UserVO;
 import com.handy.support.service.User.IUserService;
 import com.handy.web.controller.UserController;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Pan on 2019/4/12.
@@ -21,9 +28,43 @@ public class UserServiceTest extends BaseTest{
         UserDto user = iUserService.getUserByID(2);
         assertTrue(user != null);
     }
+    @Test
+    public void addUserTest(){
+        String msg = iUserService.addUser("1234@qq.com","1234");
+        assertTrue(msg.equals("success"));
+    }
+    @Test
+    public void getUserByEmailTest(){
+        User user = iUserService.getUserByEmail("1234@qq.com");
+        assertFalse(user==null);
+    }
+    @Test
+    public void updateUserTest(){
+        UserDto dto = new UserDto();
+        dto.setUserId(232);
+        dto.setSex((byte)1);
+        String msg = iUserService.updateUser(dto);
+        assertTrue(msg.equals("success"));
+    }
 
     @Test
-    public void loginTest(){
-        UserController controller = new UserController();
+    public void addUserLabelsTest(){
+        int uid = 232;
+        List<Integer> labels = new ArrayList<Integer>();
+        labels.add(1);
+        labels.add(2);
+        String msg = iUserService.addUserLabels(uid,labels);
+        assertTrue(msg.equals("success"));
     }
+
+    @Test
+    public void revert2VOTest(){
+        User user = new User();
+        user.setUserId(4);
+        user.setEmail("123@qq.com");
+        UserVO vo = iUserService.revert2VO(user);
+        assertTrue(vo != null);
+    }
+
+
 }
