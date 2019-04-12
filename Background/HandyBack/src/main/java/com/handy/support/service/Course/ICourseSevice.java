5ef9e7ffe8de6a6c94b4fe283be771a5ece2466b
Panda98 <810650217@qq.com>
2019-04-12 10:46:29 +0800

    重建

--- Background/HandyBack/src/main/java/com/handy/support/service/Course/ICourseSevice.java
+++
@@ -1,9 +0,0 @@
-package com.handy.support.service.Course;
-
-import com.handy.support.entity.Course;
-/**
- * Created by joanie on 2019/4/11.
- */
-public interface ICourseSevice {
-    Course getCourseByID(String courseId);
-}
