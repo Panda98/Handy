500d7068237b926af0403d0527666e47452dfef5
2016302580101 <31274146+2016302580101@users.noreply.github.com>
2019-04-14 15:30:13 +0800

    接口更新

--- Background/HandyBack/src/main/java/com/handy/web/controller/CommentController.java
+++ Background/HandyBack/src/main/java/com/handy/web/controller/CommentController.java
@@ -1,12 +1,22 @@
 package com.handy.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/comment",produces = "application/json; charset=utf-8")
 public class CommentController {
-   public String getCourseComment(int course_id,int from,int to){
-
+    @RequestMapping(value="/all",method = RequestMethod.GET)
+   public String getCourseComment(int course_id,int page_no,int n){
+return null;
    }
+    @RequestMapping(value="/reply",method = RequestMethod.GET)
+    public String getCommentReply(int comment_id,int page_no,int n){
+        return null;
+    }
+    @RequestMapping(value="/push",method = RequestMethod.POST)
+    public String getCommentPush(int uid,int comment_id,int page_no,int n){
+        return null;
+    }
 }
