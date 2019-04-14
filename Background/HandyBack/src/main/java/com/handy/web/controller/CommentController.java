170187e8f5d05c3746bfcf23fdbef8c49d283fc7
2016302580101 <31274146+2016302580101@users.noreply.github.com>
2019-04-14 16:51:00 +0800

    comment vo Controller更新

--- Background/HandyBack/src/main/java/com/handy/web/controller/CommentController.java
+++ Background/HandyBack/src/main/java/com/handy/web/controller/CommentController.java
@@ -1,22 +1,33 @@
 package com.handy.web.controller;
 
+import com.handy.support.pojo.comment.vo.ComPush;
+import com.handy.support.pojo.comment.vo.ComRepReq;
+import com.handy.support.pojo.comment.vo.CourseComReq;
+import com.handy.support.pojo.comment.vo.RepComReq;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
 @RequestMapping(value="/comment",produces = "application/json; charset=utf-8")
 public class CommentController {
-    @RequestMapping(value="/all",method = RequestMethod.GET)
-   public String getCourseComment(int course_id,int page_no,int n){
+    @RequestMapping(value="/all",method = GET)
+   public String getCourseComment(CourseComReq req){
 return null;
    }
-    @RequestMapping(value="/reply",method = RequestMethod.GET)
-    public String getCommentReply(int comment_id,int page_no,int n){
+    @RequestMapping(value="/replyComment",method = GET)
+    public String getCommentReply(ComRepReq req){
         return null;
     }
     @RequestMapping(value="/push",method = RequestMethod.POST)
-    public String getCommentPush(int uid,int comment_id,int page_no,int n){
+    public String pushComment(ComPush req){
+        return null;
+    }
+    @RequestMapping(value="/reply",method = GET)
+    public String replyComment(RepComReq req){
         return null;
     }
 }
+
