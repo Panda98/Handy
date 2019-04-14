a62afb1ade24278257c5d764a0539295dfc2627c
2016302580101 <31274146+2016302580101@users.noreply.github.com>
2019-04-14 14:55:09 +0800

    添加接口

---
+++ Background/HandyBack/src/main/java/com/handy/support/service/Comment/ICommentService.java
@@ -0,0 +1,9 @@
+package com.handy.support.service.Comment;
+
+import com.handy.support.entity.Comment;
+
+import java.util.List;
+
+public interface ICommentService {
+     List<Comment> getCourseComment(int course_id, int from, int to);
+}
