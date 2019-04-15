963e1f63a2b234f6ffb0da246371aceeb000e7e6
王子昂 <gnaizgnaw@163.com>
2019-04-15 12:23:18 +0800

    文件冲突

--- APP/HANDY/app/src/main/java/com/example/handy/view/activity/LoginActivity.java
+++ APP/HANDY/app/src/main/java/com/example/handy/view/activity/LoginActivity.java
@@ -16,6 +16,7 @@ import com.example.handy.presenter.LoginPresenter;
 import com.example.handy.utils.CommonUtils;
 import com.example.handy.utils.StatusBarUtil;
 import com.jakewharton.rxbinding2.view.RxView;
+import com.squareup.haha.perflib.Main;
 
 import java.util.concurrent.TimeUnit;
 
@@ -58,6 +59,7 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements Login
     @Override
     public void showLoginSuccess() {
         CommonUtils.showMessage(this, getString(R.string.login_success));
+        startActivity(new Intent(this, MainActivity.class));
         onBackPressedSupport();
     }
 
