6f6bae7dc49fc344cf7fbf7785b4f9d1d40e3532
misssfinch <31304404+misssfinch@users.noreply.github.com>
2019-05-03 14:19:13 +0800

    init

--- APP/HANDY/app/src/main/java/com/example/handy/contract/AccountPagerContract.java
+++ APP/HANDY/app/src/main/java/com/example/handy/contract/AccountPagerContract.java
@@ -23,6 +23,25 @@ public interface AccountPagerContract {
         void loadMore();
 
         /**
+         * Get User Info 加载用户数据
+         *
+         * @param isShowError If show error
+         */
+        void getUserInfo(boolean isShowError);
+
+        /**
+         * Get My Course 加载我的教程信息
+         *
+         * @param isShowError If show error
+         */
+        void getMyCourse(boolean isShowError);
+
+        /**
+         * Get My Album 加载我的专辑信息
+         */
+        void getMyAlbum(boolean isShowError);
+
+        /**
          * Load more data 显示更多数据
          */
         void loadMoreData();
