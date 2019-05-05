ee56ed3d727c1bcbe295a686a3dd056c4df8a1bc
joanie10256 <1466913846qq.com>
2019-05-05 17:02:11 +0800

    nothing

---
+++ Background/HandyBack/src/main/java/com/handy/support/mapper/iMapper/ILabelMapper.java
@@ -0,0 +1,12 @@
+package com.handy.support.mapper.iMapper;
+
+import com.handy.support.entity.Label;
+
+import java.util.List;
+
+/**
+ * Created by joanie on 2019/5/5.
+ */
+public interface ILabelMapper {
+    List<Label> getAll();
+}
--- Background/HandyBack/src/main/java/com/handy/web/controller/CourseController.java
+++ Background/HandyBack/src/main/java/com/handy/web/controller/CourseController.java
@@ -230,4 +230,6 @@ public class CourseController {
     }
 
 
+
+
 }
