fb07ab124073c5ead02034dd1fd3bd4f7c9cb507
2016302580101 <31274146+2016302580101@users.noreply.github.com>
2019-05-05 23:07:03 +0800

    撤销aop message修改

--- Background/HandyBack/src/main/java/com/handy/support/mapper/customMapper/MyRecommendMapper.java
+++ Background/HandyBack/src/main/java/com/handy/support/mapper/customMapper/MyRecommendMapper.java
@@ -3,6 +3,7 @@ package com.handy.support.mapper.customMapper;
 import com.handy.support.pojo.recommend.dto.UserItemLike;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface MyRecommendMapper {
@@ -10,4 +11,5 @@ public interface MyRecommendMapper {
     void updateRecord(UserItemLike item);
     UserItemLike selectRecord(@Param("userId") int uid, @Param("itemId") int item);
     List<Integer> getCoursesFromAlbum(@Param("albumId")int albumId,@Param("startRow") int page_no,@Param("size") int n);
+    List<UserItemLike> getUpdates(@Param("updateTime")String updateTime);
 }
