8be0d1fbd89d32b9d32730494218506db765c5f1
misssfinch <31304404+misssfinch@users.noreply.github.com>
2019-05-06 20:54:12 +0800

    message

--- APP/HANDY/app/src/main/java/com/example/handy/contract/MessageContract.java
+++ APP/HANDY/app/src/main/java/com/example/handy/contract/MessageContract.java
@@ -3,6 +3,7 @@ package com.example.handy.contract;
 import com.example.handy.base.presenter.AbstractPresenter;
 import com.example.handy.base.view.AbstractView;
 import com.example.handy.core.bean.AlbumCoverData;
+import com.example.handy.core.bean.CommentMessageData;
 
 import java.util.List;
 
@@ -10,9 +11,11 @@ public interface MessageContract {
 
     interface View extends AbstractView {
 
+
     }
 
     interface Presenter extends AbstractPresenter<MessageContract.View> {
+
 
     }
 }
--- APP/HANDY/app/src/main/java/com/example/handy/contract/message/ReceivedCommentPagerContract.java
+++ APP/HANDY/app/src/main/java/com/example/handy/contract/message/ReceivedCommentPagerContract.java
@@ -2,14 +2,25 @@ package com.example.handy.contract.message;
 
 import com.example.handy.base.presenter.AbstractPresenter;
 import com.example.handy.base.view.AbstractView;
+import com.example.handy.core.bean.CommentMessageData;
+
+import java.util.List;
 
 public interface ReceivedCommentPagerContract {
 
     interface View extends AbstractView {
+        void showReceiveMessage(List<CommentMessageData> commentMessageData, boolean isRefresh);
 
     }
 
     interface Presenter extends AbstractPresenter<ReceivedCommentPagerContract.View> {
+        void getReceiveMessage(boolean isShowError);
+
+        void autoRefresh(boolean isShowError);
+
+        public void loadMore();
+
+        public void loadMoreData();
 
     }
 }
