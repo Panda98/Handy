bfa31643513cd8b6af691e69bc8a476e2ba43fc0
王子昂 <gnaizgnaw@163.com>
2019-05-06 23:21:49 +0800

    完成根据标签显示教程

--- APP/HANDY/app/src/main/java/com/example/handy/view/fragment/message/ReceivedLikeFragment.java
+++ APP/HANDY/app/src/main/java/com/example/handy/view/fragment/message/ReceivedLikeFragment.java
@@ -11,8 +11,11 @@ import com.example.handy.R;
 import com.example.handy.app.Constants;
 import com.example.handy.base.fragment.BaseRootFragment;
 import com.example.handy.contract.message.ReceivedCommentPagerContract;
+import com.example.handy.core.bean.CommentMessageData;
 import com.example.handy.presenter.message.ReceivedCommentPagerPresenter;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -39,4 +42,8 @@ public class ReceivedLikeFragment extends BaseRootFragment<ReceivedCommentPagerP
         return fragment;
     }
 
+    @Override
+    public void showReceiveMessage(List<CommentMessageData> commentMessageData, boolean isRefresh) {
+
+    }
 }
